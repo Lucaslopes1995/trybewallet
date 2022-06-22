7b38f4e332ba11582e6af069ce40b3ff6226cf9d
Lucas <lucaslopes@gea.inatel.br>
2022-06-22 16:45:51 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
    renderRoutes = () => (
      <Switch>
 
-       <Route path="/" component={ Home } />
+       <Route exact path="/" component={ Login } />
+       <Route exact path="/carteira" component={ Wallet } />
        ;
      </Switch>
    );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,90 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { sendLogin } from '../actions';
+
+const QTD_CHARACTERES = 6;
 
 class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+    state = {
+      email: '',
+      senha: '',
+    }
+
+    handleChange = ({ target }) => {
+      const { name, value } = target;
+      this.setState({ [name]: value });
+    }
+
+    validButton = () => {
+      const { email, senha } = this.state;
+      const exRegEmail = /^.+@.+\..+$/.test(email);
+      let aux = true;
+      if (exRegEmail && senha.length >= QTD_CHARACTERES) {
+        aux = false;
+      }
+      return aux;
+    }
+
+    handleSumit = (e) => {
+      const { email, senha } = this.state;
+      const { login, history } = this.props;
+      e.preventDefault();
+      login({ email, senha });
+      history.push('/carteira');
+    }
+
+    render() {
+      const { email, senha } = this.state;
+      const validButton = this.validButton();
+      return (
+        <div>
+          <form onSubmit={ this.handleSumit }>
+
+            <label htmlFor="email-home">
+              Email
+              <input
+                name="email"
+                value={ email }
+                onChange={ this.handleChange }
+                type="email"
+                data-testid="email-input"
+                id="email-home"
+              />
+
+            </label>
+            <label htmlFor="senha-home">
+              senha
+              <input
+                name="senha"
+                value={ senha }
+                onChange={ this.handleChange }
+                type="password"
+                data-testid="password-input"
+                id="senha-home"
+              />
+
+            </label>
+            <button
+              type="submit"
+              disabled={ validButton }
+            >
+              Entrar
+
+            </button>
+          </form>
+        </div>
+      );
+    }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  login: (userdata) => { dispatch(sendLogin(userdata)); },
+});
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  history: PropTypes.node.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
