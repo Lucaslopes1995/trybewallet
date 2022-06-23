1fdf361115bdc2484c8f6670ac7d89f241c217e1
Lucas <lucaslopes@gea.inatel.br>
2022-06-23 01:12:00 -0300

    requisito 8

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Table extends React.Component {
   render() {
+    const { despesas } = this.props;
+    // const { exchangeRates } = despesas[0];
+    console.log(despesas);
     return (
       <table>
         <tr>
@@ -17,20 +20,34 @@ class Table extends React.Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        {despesas?.map((el, index) => (
+          <tr key={ index + el.id }>
+            <td>{el.description}</td>
+            <td>{el.tag}</td>
+            <td>{el.method}</td>
+            <td>{parseFloat(el.value).toFixed(2)}</td>
+            <td>{ el.exchangeRates[el.currency].name}</td>
+            <td>{parseFloat(el.exchangeRates[el.currency].ask).toFixed(2)}</td>
+            <td>
+              {(parseFloat(el.exchangeRates[el.currency].ask)
+            * parseFloat(el.value)).toFixed(2)}
+
+            </td>
+            <td>Real</td>
+            <td>{el.description}</td>
+
+          </tr>
+        ))}
       </table>
     );
   }
 }
 const mapStateToProps = (state) => ({
-  email: state.user.email,
-  expenses: state.wallet.expenses,
+  despesas: state.wallet.expenses,
 });
 
-// Table.propTypes = {
-//   getcurrencies: PropTypes.func.isRequired,
-//   fetchGasto: PropTypes.func.isRequired,
-//   currencies: PropTypes.node.isRequired,
-
-// };
+Table.propTypes = {
+  despesas: PropTypes.node.isRequired,
+};
 
 export default connect(mapStateToProps)(Table);
